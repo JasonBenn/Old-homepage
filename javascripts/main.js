273068ac7332fc81de63795ac11541fc61a8684e
Jason Benn <jasoncbenn@gmail.com>
2013-08-03 22:19:19 -0500

    Add projects slideshow.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
 	$('.recommendations, #modal-overlay').click(toggleModal)
+	hideSlideshowExceptFirstProject()
+	startSlideshow()
 })
 
 var toggleModal = function() {
 	$('#modal-overlay').fadeToggle();
 	$('#recommendations-modal').toggle();
+function hideSlideshowExceptFirstProject() {
+	$('.projects li').slice(1).hide()
+}
+
+function startSlideshow() {
+	var timer = setInterval(function() {
+		$('.projects li').first().appendTo('.projects').fadeOut(1000)
+		$('.projects li').first().fadeIn(1000)
+	}, 6000)
 }
