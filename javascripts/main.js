6dd02bee629d0eb01ce64410440f4ee0456a55d7
Jason Benn <jasoncbenn@gmail.com>
2013-08-20 02:17:00 -0400

    Refactor JS.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,13 @@
 $(document).ready(function() {
-	$('.recommendations').click('#recommendations-modal', showModal)
-	$('.books').click('#books-modal', showModal)
-	$('#modal-overlay').click(hideModal)
-	hideSlideshowExceptFirstProject()
-	startSlideshow()
+	$('.recommendations, #modal-overlay').click(toggleModal)
+	$('.accordion-header').click(accordion)
 })
 
-var showModal = function(modalSelector) {
-	$('#modal-overlay').fadeIn()
-	$(modalSelector.data).show()
+function toggleModal() {
+	$('#modal-overlay, #recommendations-modal').fadeToggle()
 }
 
-var hideModal = function() {
-	$('#modal-overlay, #recommendations-modal, #books-modal').fadeOut()
-}
-
-function hideSlideshowExceptFirstProject() {
-	$('.projects li').slice(1).hide()
-}
-
-function startSlideshow() {
-	var timer = setInterval(function() {
-		$('.projects li').first().appendTo('.projects').fadeOut(1000)
-		$('.projects li').first().fadeIn(1000)
-	}, 6000)
+function accordion() {
+	$('.accordion-content').hide();
+	$(this).next().show()
 }
