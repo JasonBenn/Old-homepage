e7f7e3536341d3996ba684c84e08f752c9879570
Jason Benn <jasoncbenn@gmail.com>
2013-08-23 00:33:17 -0700

    Caption and image are replaced on click.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.recommendations, #modal-overlay').click(toggleModal)
 	$('.accordion-header').click(accordion)
+	$('.accordion-content li').click(showDetails)
 })
 
 function toggleModal() {
@@ -9,4 +10,14 @@ function toggleModal() {
 
 function accordion() {
 	$(this).next().slideToggle()
+}
+
+function showDetails() {
+	var contentSelector = '.all-captions .' + $(this).attr('data-content')
+	image = $(contentSelector + ' .image').clone()
+	caption = $(contentSelector + ' .caption-html').clone()
+	$('.macbook-caption, .macbook .screen span').fadeOut(function() {
+		$('.macbook .screen span').html(image).fadeIn()
+		$('.macbook-caption').html(caption).fadeIn()
+	})
 }
